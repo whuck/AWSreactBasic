ad30d88a08910ca92b1e64c273cd2541eb06fda1
whuck <70232597+whuck@users.noreply.github.com>
2021-09-05 19:39:24 -0500

    assignment 2 done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
 // Author:      will huck
 // Version:     0.3.5
 import React, { Component } from 'react';
-import Country from './components/Country';
+import Countries from './components/Countries';
 
 // App.css should be imported after all child components
 import './App.css';
 
 class App extends Component {
-  state = { 
-    countries: [
-      {id:0,name:'usa',gold:1},
-      {id:1,name:'china',gold:0}
-    ]
-  }
-
 
   render() { 
-    const {countries} = this.state;
     return ( 
       <div className="App">
         <header className="App-header">
           huck's awesome medal app
         </header>
-        { countries.map(country => 
-          <Country 
-            key={ country.id } 
-            country = { country }
-          />) }
+        <Countries></Countries>
       </div>
      );
   }
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import {Button} from '@material-ui/core';
 import {AddCircle} from '@material-ui/icons';
-
+import {RemoveCircle} from '@material-ui/icons';
 class Country extends Component {
     state = {
         name: this.props.country.name == null ? "" : this.props.country.name,
         gold: this.props.country.gold == null ? 0 : this.props.country.gold
     }
-    handleIncrement = () => {
+    handleIncrement = (i) => {
         console.log("handle increment["+this.state.name+"]");
         var g = this.state.gold;
-        g++;
+        g+=i;
+        g = g < 0 ? 0 : g;
         this.setState({gold:g});
     }
     render() {
@@ -18,7 +19,8 @@ class Country extends Component {
             <div className="country">
                 <div className="name">{this.state.name}</div>
                 <div className="medals">{this.state.gold}</div>
-                <Button color="info" className="incrementMedalBtn" onClick={ () => this.handleIncrement() }><AddCircle color="primary"></AddCircle></Button>
+                <Button className="incrementMedalBtn" onClick={ () => this.handleIncrement(1) }><AddCircle color="primary"></AddCircle></Button>
+                <Button className="incrementMedalBtn" onClick={ () => this.handleIncrement(-1)}><RemoveCircle color="secondary"></RemoveCircle></Button>
             </div>
 
         );
